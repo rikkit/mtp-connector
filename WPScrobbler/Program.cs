2f4ddad1b184aad834ea5a55677cffd84c82e567
Rikki Tooley <hello@rikk.it>
2012-12-18 12:00:26 +0000

    Can enumerate multiple devices successfully, see http://stackoverflow.com/a/6166487/268555

---
+++ WPScrobbler/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using PortableDeviceApiLib;
+
+namespace WPScrobbler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new PortableDeviceCollection();
+
+            collection.Refresh();
+
+            foreach (var device in collection)
+            {
+                Console.WriteLine(device.DeviceId);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+public class PortableDeviceCollection : Collection<PortableDevice>
+{
+    private readonly PortableDeviceManager _deviceManager;
+
+    public PortableDeviceCollection()
+    {
+        this._deviceManager = new PortableDeviceManager();
+    }
+
+    public void Refresh()
+    {
+        this._deviceManager.RefreshDeviceList();
+
+        // Determine how many WPD devices are connected
+        uint count = 0;
+        this._deviceManager.GetDevices(null, ref count);
+
+        // Retrieve the device id for each connected device
+        var deviceIds = new string[count];
+        this._deviceManager.GetDevices( deviceIds, ref count);
+        foreach (var deviceId in deviceIds)
+        {
+            Add(new PortableDevice(deviceId));
+        }
+    }
+}
+
+public class PortableDevice
+{
+    public PortableDevice(string deviceId)
+    {
+        this.DeviceId = deviceId;
+    }
+
+    public string DeviceId { get; set; }
+}
